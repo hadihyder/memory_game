e7ea1bc49e07fb88671c535c60cdd38b46152990
Hadi Hyder <hyderhadi123@gmail.com>
2021-09-07 17:26:44 -0500

    Initial commit

---
+++ app/src/main/java/com/hadihyder/memorygame/models/UserImageList.kt
@@ -0,0 +1,7 @@
+package com.hadihyder.memorygame.models
+
+import com.google.firebase.firestore.PropertyName
+
+data class UserImageList(
+        @PropertyName("images")val images:List<String>?=null
+)
---
+++ app/src/main/java/com/hadihyder/memorygame/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.hadihyder.memorygame.utils
+
+import com.hadihyder.memorygame.R
+const val EXTRA_GAME_NAME="EXTRA_GAME_NAME"
+const val EXTRA_BOARD_SIZE="EXTRA_BOARD_SIZE"
+val DEFAULT_ICONS= listOf(
+    R.drawable.ic_aeroplane,
+    R.drawable.ic_bag,
+    R.drawable.ic_camera,
+    R.drawable.ic_car,
+    R.drawable.ic_fire,
+    R.drawable.ic_flower,
+    R.drawable.ic_gas,
+    R.drawable.ic_lock,
+    R.drawable.ic_mail,
+    R.drawable.ic_man,
+    R.drawable.ic_pizza,
+    R.drawable.ic_tv
+)
